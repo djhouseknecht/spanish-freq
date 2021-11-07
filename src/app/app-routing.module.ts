91dffe7370f65318a759590cd99d9f33e0599ef3
David Houseknecht <david.j.houseknecht@gmail.com>
2021-11-07 17:28:56 -0500

    Added about page and fixed deploy dir path

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,10 @@ import { RouterModule, Routes } from '@angular/router';
 import { MainComponent } from './main/main.component';
 import { LemmaComponent } from './lemma/lemma.component';
 import { WordComponent } from './word/word.component';
+import { AboutComponent } from './about/about.component';
 
 const routes: Routes = [
-  // { path: 'lemmas', component: MainComponent },
-  // { path: 'conjugates', component: MainComponent },
+  { path: 'about', component: AboutComponent },
   { path: 'word/:word', component: WordComponent },
   { path: 'lemma/:word', component: LemmaComponent },
   { path: '', component: MainComponent,/*  pathMatch: 'full' */ },
