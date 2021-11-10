5ae30e0f31f9a699ca24016e85d7e68e24c92040
David Houseknecht <david.j.houseknecht@gmail.com>
2021-11-10 08:07:07 -0500

    Added animations to loading spinner

--- src/app/shared/loading/loading.component.ts
+++ src/app/shared/loading/loading.component.ts
@@ -1,10 +1,28 @@
+import { trigger, state, style, transition, animate } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
 import { LoadingService } from '../../core/loading.service';
 
 @Component({
   selector: 'sf-loading',
   templateUrl: './loading.component.html',
-  styleUrls: ['./loading.component.scss']
+  styleUrls: ['./loading.component.scss'],
+  animations: [
+    trigger('loadingNotLoading', [
+      state('loading', style({
+        opacity: 1,
+      })),
+      state('no-loading', style({
+        opacity: 0.2,
+        display: 'none',
+      })),
+      transition('loading => no-loading', [
+        animate('.3s')
+      ]),
+      transition('no-loading => loading', [
+        animate('.1s')
+      ]),
+    ]),
+  ],
 })
 export class LoadingComponent implements OnInit {
   isLoading = false;
@@ -14,5 +32,11 @@ export class LoadingComponent implements OnInit {
   ngOnInit (): void {
     this.loadingService.isLoading$
       .subscribe(loading => this.isLoading = loading);
+
+    // timer(900).pipe(
+    //   this.loadingService.startLoading('haya'),
+    //   switchMap(() => timer(3000)),
+    //   this.loadingService.stopLoading('haya'),
+    // ).subscribe()
   }
 }
